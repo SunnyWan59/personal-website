7c0e9261a6b065534ce2e3dfba74877341d397be
Sunny Wan <94445569+SunnyWan59@users.noreply.github.com>
2024-02-22 01:39:19 -0500

    Update bookshelf.js

--- src/pages/Bookshelf/bookshelf.js
+++ src/pages/Bookshelf/bookshelf.js
@@ -6,6 +6,8 @@ function Bookshelf(){
     return(
         <div>
             <Navbar/>
+            <div id="title"> Bookshelf</div>
+            <hr width="100%" size="3" color="grey"></hr>
         </div>
     )
 }
